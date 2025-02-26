f6838552adaaa87dd9ab37a0c705a7ef99ce7f3e
Aviral Meharishi <aviralmeharishi@gmail.com>
2025-02-27 00:15:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def prediction(input_list):
     pred = model.predict_proba([input_list])[:,1][0]
 
 
-    if pred > 0.5:
+    if pred > 0.25:
                 st.failure(f'''⚠️ You Have More Chances of Getting Diseased
     Your Probability Of Having Cardiovascular Disease is {round(pred, 2)}
     Take Care!''')
